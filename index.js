967e87870501871c33b9c49379b9dbd567677ce9
Dane David <dane@surveysparrow.com>
2020-05-20 23:07:29 +0530

    Add unblock() & stay()

--- index.js
+++ index.js
@@ -9,28 +9,46 @@ const useNavPrompt = ({
   shouldBlock,
 }) => {
   const history = useHistory();
-  const unblock = useRef(null);
+
+  const _unblock = useRef(null);
+  const _navPath = useRef(null);
+
   const [blocked, setBlocked] = useState(false);
 
   useEffect(() => {
     if ( shouldBlock ) {
-      unblock.current = history.block(() => {
+      _unblock.current = history.block((location) => {
         // need to use ref?
-        setBlocked(true)
-        console.log('Blocked');
+        setBlocked(true);
+        _navPath.current = location.pathname;
         return false;
       });
 
       return () => {
-        if ( unblock.current)  {
-          unblock.current();
-          return false;
+        if ( _unblock.current)  {
+          _unblock.current();
         }
       };
     }
   }, [shouldBlock]);
 
-  return blocked;
+  const unblock = () => {
+    if ( _unblock.current ) {
+      setBlocked(false);
+      _unblock.current();
+      history.push(_navPath.current);
+    }
+  };
+
+  const stay = () => {
+    setBlocked(false);
+  };
+
+  return {
+    blocked,
+    unblock,
+    stay,
+  };
 }
 
 export default useNavPrompt;
