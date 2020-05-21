cc26646adae12661ef877179fdbb605394f1081c
Dane David <dane@surveysparrow.com>
2020-05-22 00:58:16 +0530

    Add flow-typed/react-router-dom. Flow coverage at 100%.

--- src/useNavPrompt.js
+++ src/useNavPrompt.js
@@ -5,25 +5,26 @@ import {
   useEffect,
 } from 'react';
 import { useHistory } from 'react-router-dom';
+
 import type { NavPromptConfig, NavPromptParams } from './types';
+import type { Location } from 'react-router-dom';
 
 const useNavPrompt = ({
   shouldBlock,
 }: NavPromptParams): NavPromptConfig  => {
   const history = useHistory();
 
-  const _unblock = useRef(null);
-  const _navPath = useRef(null);
+  const _unblock = useRef<null | () => void>(null);
+  const _navPath = useRef<null | string>(null);
 
   const [blocked, setBlocked] = useState(false);
 
   useEffect(() => {
     if ( shouldBlock ) {
-      _unblock.current = history.block((location) => {
+      _unblock.current = history.block((location: Location) => {
         // need to use ref?
         setBlocked(true);
         _navPath.current = location.pathname;
-        return false;
       });
 
       return () => {
@@ -38,7 +39,7 @@ const useNavPrompt = ({
     if ( _unblock.current ) {
       _unblock.current();
       setBlocked(false);
-      history.push(_navPath.current);
+      _navPath.current && history.push(_navPath.current);
     }
   };
 
